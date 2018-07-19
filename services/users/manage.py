3cd125e3af7a688c3b27171cdd579a6506493410
IPconfig <justin.haan92@gmail.com>
2018-07-19 21:04:42 +0200

    add tests

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,16 +1,30 @@
 # services/users/manage.py
 
+import unittest
+
 from flask.cli import FlaskGroup
 from project import app, db
 
-# Create a new FlaskGroup instance to extend normal CLI with commands related to the Flask app
+# Extend normal CLI with commands related to the Flask app
 cli = FlaskGroup(app)
+
 
 @cli.command()
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """Runs the tests withouth code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
 
 if __name__ == '__main__':
     cli()
