39b4994c3712ae374e42b6c2cb19e7cf144c424b
IPconfig <justin.haan92@gmail.com>
2018-07-19 20:21:34 +0200

    add Postgres database

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,10 +1,16 @@
 # services/users/manage.py
 
 from flask.cli import FlaskGroup
-from project import app
+from project import app, db
 
 # Create a new FlaskGroup instance to extend normal CLI with commands related to the Flask app
 cli = FlaskGroup(app)
+
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
