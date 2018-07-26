a57f2efdd9e9bb7998bc2bacf94333b401fa5531
IPconfig <justin.haan92@gmail.com>
2018-07-27 00:57:22 +0200

    update CLI command
    seed_db

--- services/users/manage.py
+++ services/users/manage.py
@@ -45,8 +45,16 @@ def test():
 @cli.command()
 def seed_db():
     """Seeds the database."""
-    db.session.add(User(username='justin', email='justin@me.com'))
-    db.session.add(User(username='ipconfig', email='ipconfig@me.com'))
+    db.session.add(User(
+                    username='justin', 
+                    email='justin@me.com', 
+                    password='greaterthaneight'
+                ))
+    db.session.add(User(
+                    username='ipconfig', 
+                    email='ipconfig@me.com', 
+                    password='greaterthaneight'
+                ))
     db.session.commit()
 
 
