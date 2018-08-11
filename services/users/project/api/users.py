76ce95a9531ae199ccc57529fffe979a995ded0c
IPconfig <justin.haan92@gmail.com>
2018-08-11 22:31:14 +0200

    fix some typo's and indentation mistakes
    All user tests pass now

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -43,7 +43,7 @@ def add_user(resp):
     }
     if not is_admin(resp):
         response_object['message'] = 'You do not have permission to do that.'
-    return jsonify(response_object), 401
+        return jsonify(response_object), 401
     if not post_data:
         return jsonify(response_object), 400
     username = post_data.get('username')
