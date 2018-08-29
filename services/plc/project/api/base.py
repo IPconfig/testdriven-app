cd7b7ba720caf2de3aa9e4c59e1b1fd8d6c17e91
IPconfig <justin.haan92@gmail.com>
2018-08-29 02:07:04 +0200

    temp new db_read function

--- services/plc/project/api/base.py
+++ services/plc/project/api/base.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, jsonify, render_template
 
-from project.api.utils import plc_connect, plc_read
+from project.api.utils import plc_connect, plc_read, plc_read_all
 
 
 plc_blueprint = Blueprint('plc', __name__, template_folder='./templates')
@@ -38,6 +38,27 @@ def overview():
     plc = plc_connect('192.168.0.1', 0, 0)
     response = plc_read(plc, 7, 0, 2002, 6002)
     return render_template('overview.html', values=response)
+
+
+@plc_blueprint.route('/plc/new', methods=['GET'])
+def new():
+    response_object = {
+        'status': 'fail',
+        'message': 'An error occured.'
+    }
+    try:
+        plc = plc_connect('192.168.0.1', 0, 0)
+        if not plc:
+            response_object['message'] = 'Could not connect to plc'
+            return jsonify(response_object), 400
+        else:
+            response = plc_read_all(plc)
+            response_object['status'] = 'success'
+            response_object['message'] = 'Successfully scraped data.'
+            response_object['values'] = response
+            return jsonify(response_object), 200
+    except Exception as e:
+        return jsonify(response_object), 400
 
 
 @plc_blueprint.route('/plc/ping', methods=['GET'])
--- services/plc/project/api/utils.py
+++ services/plc/project/api/utils.py
@@ -45,7 +45,7 @@ def plc_read(client, db_num, db_offset_start, db_offset_end, db_values_offset):
     db_values_offset : Int
         Offset on which data values appear, after the tubes per row are defined
     """
-
+    # returns a bytearray
     tubes_data = client.db_read(db_num, db_offset_start, db_offset_end)
     decoded_tubes_per_row = [int.from_bytes(
         tubes_data[i:i + 2], byteorder='big')
@@ -73,3 +73,36 @@ def plc_read(client, db_num, db_offset_start, db_offset_end, db_values_offset):
         tube_row_no = tube_row_no + 1
         data.append(decoded_tubestate_data)
     return data
+
+
+def plc_read_all(client):
+    # returns an array with an arrary of values per row
+    # ex of 2 rows with 4 values each: [[1,2,2,3], [4,4,3,1]]
+
+    tubes_per_row_raw = client.db_read(7, 0, 2002)
+    # decoded_tubes_per_row is something like [10,10,10,10] for a 4x10 matrix
+    # 0 values get filtered, so there should be no empty rows between elements
+    tubes_per_row_decoded = [int.from_bytes(
+        tubes_per_row_raw[i:i + 2], byteorder='big')
+                            for i in range(0, len(tubes_per_row_raw), 2)
+                            if int.from_bytes(
+                                tubes_per_row_raw[i:i + 2],
+                                byteorder='big') != 0]
+
+    tubestate_start = 42002  # actual values start from this offset
+    tube_row_no = 1
+    tube_row_values = []
+    for tubes in tubes_per_row_decoded:
+        tubes_size = tubes * 2  # one element takes 2 bytes
+        tube_row_values_raw = client.db_read(7, tubestate_start, tubes_size)
+        # returns an array with all values per row
+        tube_row_values_decoded = [int.from_bytes(
+            tube_row_values_raw[i:i + 2], byteorder='big')
+            for i in range(0, len(tube_row_values_raw), 2)]
+#       print('row {}: {}'.format(tube_row_no, tube_row_values_decoded))
+
+        tubestate_start = tubestate_start + tubes_size  # go to next row
+        tube_row_no = tube_row_no + 1  # increase row number
+        tube_row_values.append(tube_row_values_decoded)
+
+    return tube_row_values
